80636582559e1b0edffad76b1cf741eb218d65fd
Sonberg <per.sonberg@gmail.com>
2019-01-05 09:58:59 +0100

    bind listners on button

--- src/components/V.Button.vue
+++ src/components/V.Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <component :is="tag || 'button'" :to="to" class="flex items-center bg-indigo-dark text-white p-3 no-underline hover:bg-indigo font-semibold text-xs uppercase"><slot/></component>
+    <component :is="tag || 'button'" :to="to" v-on="$listeners" v-bind="$props" class="flex items-center bg-indigo-dark text-white p-3 no-underline hover:bg-indigo font-semibold text-xs uppercase"><slot/></component>
 </template>
 
 <script>
@@ -7,6 +7,10 @@ export default {
     props: {
         tag: String,
         to: {}
+    },
+    created() {
+        console.log(this);
+        
     }
 }
 </script>
